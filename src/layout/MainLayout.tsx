c5b3a6364d9d413d0aaf4c74771945a3223890f5
Thittawin <ptkk00177@gmail.com>
2025-08-16 13:50:45 +0700

    completed

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -20,7 +20,7 @@ export default function MainLayout() {
           </main>
 
           {/* Footer ด้านล่าง */}
-          <Footer year="2026" fullName="chanadda" studentId="67062039" />
+          <Footer year="2026" fullName="Thittawin" studentId="670610706" />
         </div>
       </div>
     </div>
